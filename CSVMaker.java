5f0d750a44087255d5fefaa3faf3d2c220bfec3a
Mateusz Jakobik <mateusz.jakobik@ucdconnect.ie>
2024-03-28 10:36:52 +0000

    Code to create random integer csv file

---
+++ CSVMaker.java
@@ -0,0 +1,31 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
+public class CSVMaker {
+    public static void main(String[] args) {
+        // Define the number of integers to generate
+        int numberOfIntegers = 100000;
+
+        // Create a StringBuilder to hold CSV content
+        StringBuilder csvContent = new StringBuilder();
+
+        // Generate and append integers to the CSV content
+        Random random = new Random();
+        for (int i = 0; i < numberOfIntegers; i++) {
+            int randomNumber = random.nextInt();
+            csvContent.append(randomNumber);
+            csvContent.append(",");
+        }
+        // Remove the last comma
+        csvContent.deleteCharAt(csvContent.length() - 1);
+
+        // Write CSV content to file
+        try (FileWriter writer = new FileWriter("integers.csv")) {
+            writer.write(csvContent.toString());
+            System.out.println("CSV file 'integers.csv' has been created successfully.");
+        } catch (IOException e) {
+            System.err.println("An error occurred while writing the CSV file: " + e.getMessage());
+        }
+    }
+}
