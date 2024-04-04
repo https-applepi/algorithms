ffcfad533afc33e0ea78a68d1080c7b2d74c45b7
Jenny Song <jenny.song@ucdconnect.ie>
2024-04-04 10:40:36 +0100

    added a test function that tests if our array is sorted.

--- Main.java
+++ Main.java
@@ -13,5 +13,22 @@ public class Main {
             a[i++] = Integer.parseInt(in.next());
         }
         in.close();
+
+        //ptpSort(a);
+        //test if is sorted, test array below.
+        int[] b = {1, 2, 3, 4, 5, 6, 7, 7, 7, 10};
+        Main main = new Main();
+        System.out.println(main.testIfSorted(b));
+
+    }
+    public boolean testIfSorted(int[] a){
+        boolean isSorted = true;
+        for (int j = 1; j < 10; j++) { // Start from index 1, comparing with the previous element
+            if (a[j] < a[j - 1]) { // If current element is smaller than previous
+                isSorted = false; // Array is not sorted
+                break; // No need to continue checking, we already know it's not sorted
+            }
+        }
+        return isSorted;
     }
 }
