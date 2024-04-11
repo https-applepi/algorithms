fb1b140ee16fb287487dcf21b64cec143ee1d5f5
Makoskelp <maxim.koloskov@ucdconnect.ie>
2024-04-11 10:35:08 +0100

    bugfixes and improvements

--- Main.java
+++ Main.java
@@ -4,34 +4,13 @@ import java.io.File;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        int arraySize, i = 0;
-        File csv = new File("integers.csv");
-        Scanner in = new Scanner(csv);
-        in.useDelimiter(",");
-        arraySize = Integer.parseInt(in.next());
-        int[] a = new int[arraySize];
-        while (in.hasNext()) {
-            a[i++] = Integer.parseInt(in.next());
-        }
-        in.close();
-
-        //ptpSort(a);
-        //test if is sorted, test array below.
-        int[] b = {1, 2, 3, 4, 5, 6, 7, 7, 7, 10};
-        Main main = new Main();
-        System.out.println(main.testIfSorted(b));
-
-    }
     public boolean testIfSorted(int[] a){
-        boolean isSorted = true;
         for (int j = 1; j < 10; j++) { // Start from index 1, comparing with the previous element
             if (a[j] < a[j - 1]) { // If current element is smaller than previous
-                isSorted = false; // Array is not sorted
-                break; // No need to continue checking, we already know it's not sorted
+                return false;
             }
         }
-        return isSorted;
+        return true; //if no inconsistencies found, return true
     }
 
     private int hoare(int[] array, int start, int end, int pivot) {
@@ -51,7 +30,27 @@ public class Main {
             j = swap;
         }
     }
+    
+    public static void main(String[] args) throws Exception {
+        int arraySize, i = 0;
+        File csv = new File("integers.csv");
+        Scanner in = new Scanner(csv);
+        in.useDelimiter(",");
+        arraySize = Integer.parseInt(in.next());
+        int[] array = new int[arraySize];
+        while (in.hasNext()) {
+            array[i++] = Integer.parseInt(in.next());
+        }
+        in.close();
+
+        //ptpSort(a);
+        //test if is sorted, test array below.
+        int[] testArray = {1, 2, 3, 4, 5, 6, 7, 7, 7, 10};
+        Main main = new Main();
+        System.out.println(main.testIfSorted(testArray));
+    }
 }
+
 class Node {
     Node left;
     Node right;
@@ -63,6 +62,7 @@ class Node {
         right = null;
     }
 }
+
 class PTPSort
 {
     public int[] PTPSort(int a, int beg, int end){
@@ -73,6 +73,7 @@ class PTPSort
             }
     }
 }
+
 class Traverse implements Runnable {
     private final Node node;
 
