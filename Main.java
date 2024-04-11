af08fcc776dce5bc5c864b3f8ddcaf7077f9ffc6
Jenny Song <jenny.song@ucdconnect.ie>
2024-04-11 11:04:00 +0100

    PTP sort started

--- Main.java
+++ Main.java
@@ -1,5 +1,4 @@
-
-
+import java.util.*;
 import java.io.File;
 import java.util.Scanner;
 
@@ -11,6 +10,20 @@ public class Main {
             }
         }
         return true; //if no inconsistencies found, return true
+    }
+    int USTL = 100;
+    boolean partitionOnly = false;
+    public void PTPSort(int[] a, int beg, int end){
+        if((end - beg +1)<USTL){
+            if(!partitionOnly){
+                Arrays.sort(a);
+                return;
+            }
+            else{
+                int len = (beg - end)/2;
+                int lowPivot, MedPivot,HighPivot;
+            }
+        }
     }
 
     private int hoare(int[] array, int start, int end, int pivot) {
@@ -30,7 +43,7 @@ public class Main {
             j = swap;
         }
     }
-    
+
     public static void main(String[] args) throws Exception {
         int arraySize, i = 0;
         File csv = new File("integers.csv");
@@ -45,78 +58,10 @@ public class Main {
 
         //ptpSort(a);
         //test if is sorted, test array below.
-        int[] testArray = {1, 2, 3, 4, 5, 6, 7, 7, 7, 10};
+        int[] testArray = {10, 2, 3, 4, 5, 6, 7, 7, 7, 10};
         Main main = new Main();
+        main.PTPSort(testArray,0,10);
         System.out.println(main.testIfSorted(testArray));
     }
 }
-
-class Node {
-    Node left;
-    Node right;
-    int data;
-
-    public Node(int data) {
-        this.data = data;
-        left = null;
-        right = null;
-    }
-}
-
-class PTPSort
-{
-    public int[] PTPSort(int a, int beg, int end){
-        if((end - beg +1)< )
-            if(!PartitionOnly){
-                OpenMP Task
-                STLSort(a+beg,a+end+1);
-            }
-    }
-}
-
-class Traverse implements Runnable {
-    private final Node node;
-
-    public Traverse(Node node) {
-        this.node = node;
-    }
-
-    @Override
-    public void run() {
-        traverse(node);
-    }
-
-    private void traverse(Node p) {
-        if (p == null)
-            return;
-
-        Thread leftThread = null;
-        Thread rightThread = null;
-
-        if (p.left != null) {
-            Traverse tLeft = new Traverse(p.left);
-            leftThread = new Thread(tLeft);
-            leftThread.start();
-        }
-
-        if (p.right != null) {
-            Traverse tRight = new Traverse(p.right);
-            rightThread = new Thread(tRight);
-            rightThread.start();
-        }
-
-        try {
-            if (leftThread != null)
-                leftThread.join(); // Wait for the left thread to finish
-            if (rightThread != null)
-                rightThread.join(); // Wait for the right thread to finish
-
-            // Do whatever processing you need to do with the current node here
-            System.out.println("Processed node: " + p.data);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-}
-
 
